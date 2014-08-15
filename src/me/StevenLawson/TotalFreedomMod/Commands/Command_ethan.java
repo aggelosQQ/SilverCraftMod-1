2b38dae197fd6cc5ce490e9d725527663887044e
Wilee999 <Infinitywilee@rocketmail.com>
2014-08-15 15:17:16 -0700

    fixed command_ethan

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ethan.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ethan.java
@@ -25,15 +25,14 @@ public class Command_ethan extends TFM_Command
             return true;
         }
 
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> WHAT THE FUCK IS WRONG WITH YOU, YOU FUCKING DUMBASS?");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON MOTHER FUCKING THIN ICE, YOU HEAR ME??");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I WILL RIP YOUR ENTIRE ASS APART, YOUR CLOSE TO PERMBAN!!!");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE MOTHER FUCKING BLOCK, IT'S PERMBAN FUCKING BITCH!!!");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I BET YOU'RE NOT EVEN LISTENING, YOU PIECE OF FUCKING SHIT! YOU DO THAT SHIT AGAIN, IT'S PERMBAN!!!");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON FUCKING THIN ICE!!!!");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE FUCKING BLOCK ITS PERMBAN ILL TELL MARK TO PERMBAN YOU!!!!!");
-    	  Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> NOW GET YOUR FUCKING SHIT TOGETHER!!!!");
-    	  return true;
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> WHAT THE FUCK IS WRONG WITH YOU, YOU FUCKING DUMBASS?");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON MOTHER FUCKING THIN ICE, YOU HEAR ME??");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I WILL RIP YOUR ENTIRE ASS APART, YOUR CLOSE TO PERMBAN!!!");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE MOTHER FUCKING BLOCK, IT'S PERMBAN FUCKING BITCH!!!");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I BET YOU'RE NOT EVEN LISTENING, YOU PIECE OF FUCKING SHIT! YOU DO THAT SHIT AGAIN, IT'S PERMBAN!!!");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON FUCKING THIN ICE!!!!");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE FUCKING BLOCK ITS PERMBAN ILL TELL MARK TO PERMBAN YOU!!!!!");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> NOW GET YOUR FUCKING SHIT TOGETHER!!!!");
         return true;
     }
 }
