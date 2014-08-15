633e4dcc7a94fabb84431f204100351d8eb6c28c
GreatRaider <radionknife@gmail.com>
2014-08-15 10:23:07 +0100

    3rrors

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_silentsetl.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_silentsetl.java
@@ -5,17 +5,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_CONSOLE)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
 @CommandParameters(description = "Silently modifies every online users WorldEdit block modification limit to 1000.", usage = "/<command>")
 public class Command_silentsetl extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_WorldEditBridge web = TFM_WorldEditBridge.getInstance();
         for (final Player player : server.getOnlinePlayers())
         {
-            web.setLimit(player, 1000);
+            TFM_WorldEditBridge.setLimit(player, 1000);
         }
         return true;
     }
