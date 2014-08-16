bd3a569e92f12f7ae9102228ebce3fcba9ef30f8
aggelosQQ <jouflis1@hotmail.com>
2014-08-16 16:31:51 +0300

    Update Command_ethan.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ethan.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ethan.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.Bukkit;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_IN_GAME)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Sic ethanmort on people!", usage = "/<command> <player>")
 public class Command_ethan extends TFM_Command
 {
@@ -31,7 +31,7 @@ public class Command_ethan extends TFM_Command
     	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE MOTHER FUCKING BLOCK, IT'S PERMBAN FUCKING BITCH!!!");
     	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I BET YOU'RE NOT EVEN LISTENING, YOU PIECE OF FUCKING SHIT! YOU DO THAT SHIT AGAIN, IT'S PERMBAN!!!");
     	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON FUCKING THIN ICE!!!!");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE FUCKING BLOCK ITS PERMBAN ILL TELL MARK TO PERMBAN YOU!!!!!");
+    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE FUCKING BLOCK ITS PERMBAN ILL TELL AGG TO PERMBAN YOU!!!!!");
     	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> NOW GET YOUR FUCKING SHIT TOGETHER!!!!");
         return true;
     }
