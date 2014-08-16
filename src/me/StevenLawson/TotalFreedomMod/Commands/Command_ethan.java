d2eb7434cc8ff8a73d2fb7425fdb2a1fdf9c70a3
John J. Miller <a784b2a1@opayq.com>
2014-08-16 19:14:26 -0400

    BukkitAPI, really?
    I personally find this command abusive, however, I will fix it up.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ethan.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ethan.java
@@ -1,9 +1,11 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.Bukkit;
 
 @CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Sic ethanmort on people!", usage = "/<command> <player>")
@@ -25,14 +27,20 @@ public class Command_ethan extends TFM_Command
             return true;
         }
 
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> WHAT THE FUCK IS WRONG WITH YOU, YOU FUCKING DUMBASS?");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON MOTHER FUCKING THIN ICE, YOU HEAR ME??");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I WILL RIP YOUR ENTIRE ASS APART, YOUR CLOSE TO PERMBAN!!!");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE MOTHER FUCKING BLOCK, IT'S PERMBAN FUCKING BITCH!!!");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> I BET YOU'RE NOT EVEN LISTENING, YOU PIECE OF FUCKING SHIT! YOU DO THAT SHIT AGAIN, IT'S PERMBAN!!!");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> YOUR ON FUCKING THIN ICE!!!!");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> IF YOU BREAK ONE MORE FUCKING BLOCK ITS PERMBAN ILL TELL AGG TO PERMBAN YOU!!!!!");
-    	Bukkit.broadcastMessage("<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> NOW GET YOUR FUCKING SHIT TOGETHER!!!!");
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + player.getName() + "WHAT THE FUCK IS WRONG WITH YOU, YOU FUCKING DUMBASS?", ChatColor.WHITE);
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "YOUR ON MOTHER FUCKING THIN ICE, YOU HEAR ME??", ChatColor.WHITE);
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "I WILL RIP YOUR ENTIRE ASS APART, YOUR CLOSE TO PERMBAN!!!", ChatColor.WHITE);
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "IF YOU BREAK ONE MORE MOTHER FUCKING BLOCK, IT'S PERMBAN FUCKING BITCH!!!", ChatColor.WHITE);
+        player.getWorld().strikeLightning(player.getLocation());
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "I BET YOU'RE NOT EVEN LISTENING, YOU PIECE OF FUCKING SHIT! YOU DO THAT SHIT AGAIN, IT'S PERMBAN!!!", ChatColor.WHITE);
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "YOUR ON FUCKING THIN ICE!!!!", ChatColor.WHITE);
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "IF YOU BREAK ONE MORE FUCKING BLOCK ITS PERMBAN ILL TELL AGG TO PERMBAN YOU!!!!", ChatColor.WHITE);
+        TFM_Util.playerMsg(sender, "<" + ChatColor.RED + "ethanmort" + ChatColor.WHITE + "> " + "NOW GET YOUR FUCKING SHIT TOGETHER!!!!!", ChatColor.WHITE);
+        player.setFireTicks(10000);
+        player.getWorld().strikeLightning(player.getLocation());        
+        player.getWorld().strikeLightning(player.getLocation());
+        player.kickPlayer(ChatColor.DARK_PURPLE + "NOW GET YOUR FUCKING SHIT TOGETHER, OR IT'S PERMBAN!!");
+
         return true;
     }
 }
