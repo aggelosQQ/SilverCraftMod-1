a80d2a71827e64110760e7b939a4367971a1a5f0
John J. Miller <a784b2a1@opayq.com>
2014-08-20 16:06:31 -0400

    Major problem patched
    Changed color format
    Added reason/message
    Added new import

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_silversay.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_silversay.java
@@ -1,6 +1,7 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -14,11 +15,17 @@ public class Command_silversay extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+                String reason = null;
+        if (args.length >= 2)
+        {
+            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+        }
        
-        TFM_Util.bcastMsg("[SilverBot] - " + player.getName + " has been warned for ", ChatColor.RED + <there  @GreatRaider >, ChatColor.GREEN + ChatColor.BOLD);
-
+       
+       
+//        TFM_Util.bcastMsg("[SilverBot] - " + player.getName + " has been warned for ", ChatColor.RED + <there  @GreatRaider >, ChatColor.GREEN + ChatColor.BOLD);
+        TFM_Util.bcastMsg(ChatColor.GRAY + "[" + ChatColor.AQUA + "SilverBot" + ChatColor.GRAY + "] " + (reason != null ? (ChatColor.GRAY + reason) : "!"));
+        
         return true;
     }
 }
-
-// 8/16/14 
