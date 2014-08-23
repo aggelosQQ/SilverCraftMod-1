9aa56933b44393ce5434a60e54001b65268457bf
aggelosQQ <jouflis1@hotmail.com>
2014-08-24 02:16:38 +0300

    If the last one had errors, check this

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_vote.java
@@ -0,0 +1,26 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+ 
+ 
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+ 
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
+@CommandParameters(description = "How to vote", usage = "/<command>")
+public class Command_givecows extends TFM_Command
+{
+   @Override
+   public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+   {
+           server.dispatchCommand(sender, "ping &2****&6Links to vote&2****");
+           server.dispatchCommand(sender, "ping &b1)  &2http://minecraft-mp.com/server-s56156");
+           server.dispatchCommand(sender, "ping &2****&6Links to vote&2****");
+           player.chat("I just saw the vote links by using /vote, you should do it aswell!");
+           TFM_Util.bcastMsg(sender.getName() + TFM_Util.randomChatColor() + " decided" + TFM_Util.randomChatColor() + " to" + TFM_Util.randomChatColor() + " give" + TFM_Util.randomChatColor() + " you" + TFM_Util.randomChatColor() + " your" + TFM_Util.randomChatColor() + " own" + TFM_Util.randomChatColor() + " pet" + TFM_Util.randomChatColor() + " cow" + TFM_Util.randomChatColor() + "!", ChatColor.AQUA);
+           
+           return true;
+}
+}
+ 
