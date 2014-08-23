d72f69650f77344f99eb79e74ffe35eb5b5e11b5
aggelosQQ <jouflis1@hotmail.com>
2014-08-24 02:13:35 +0300

    Update TFM_PlayerListener.java

--- src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
+++ src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
@@ -886,7 +886,7 @@ public class TFM_PlayerListener implements Listener
             event.setJoinMessage(ChatColor.BLUE + "aggelosQQ - The Owner of this server has joined!");
             TFM_EssentialsBridge.setNickname(player.getName(), ChatColor.DARK_RED + "ag" + ChatColor.RED + "ge" + ChatColor.DARK_BLUE + "lo" + ChatColor.BLUE + "sQ" + ChatColor.GREEN + "Q");
             TFM_PlayerData.getPlayerData(player).setTag("&8[&9Owner&8]");
-            player.chat(ChatColor.AQUA + " If you like this server please vote at: " + ChatColor.BLUE + "http://minecraft-mp.com/server-s56156");
+            player.chat("If you like this server please vote at: http://minecraft-mp.com/server-s56156");
         }
         else if (player.getName().equalsIgnoreCase("PieGuy7896"))
         {
